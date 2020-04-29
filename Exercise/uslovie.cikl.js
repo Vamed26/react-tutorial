553485dbc7d6b0d49c4950c0ee76c3587333f5d6
Vadym <vamed8926@gmail.com>
2020-04-29 21:24:56 +0300

    Циклы и условия

---
+++ Exercise/uslovie.cikl.js
@@ -0,0 +1,58 @@
+// var name = "Володя";
+// console.log ("Привет, "+ name);
+// if (name.length >6 ) {
+//     console.log ("Ну и длинющее у тебя имя");
+// }
+//     else {
+//     console.log ("Твое имя не самое длинное")
+// }
+// var name = "Вася";
+// if (name === "Вадим" ) {
+//     console.log ("Привет мне");
+// }
+//     else {
+//     console.log ("Привет незнакомец")
+// }
+// for (var x =3; x<10000; x=x*3) {
+//     console.log(x);
+// }
+// var x =3; 
+// while (x<10000) {
+//     console.log(x);
+//     x=x*3;
+// }
+//  var animals = ["Кот","Рыба", "Лемур", "Комодский варан"].join("-прекрасное животное ");
+// console.log(animals);
+// Мой вариант^
+// var animals = ["Кот","Рыба", "Лемур", "Комодский варан"];
+//  animals[0]= animals[0] +" -прекрасное животное";
+//  animals[1]= animals[1] +" -прекрасное животное";
+//  animals[2]= animals[2] +" -прекрасное животное";
+//  animals[3]= animals[3] +" -прекрасное животное";
+//  console.log(animals);
+// СЛОЖНОЕ ЗАДАНИЕ
+// var alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+// var randomString="";
+//  while (randomString.length<8) {
+
+//      randomString+=alphabet[Math.floor(Math.random()*alphabet.length)];
+//  }
+//  console.log (randomString);
+var input = "javascript is awesome";
+var output = "";
+for (var i = 0; i < input.length; i++) {
+    output += input[i];
+    if (input[i] === "a") {
+        output = output.replace('a', 4);
+    }
+    if (input[i] === "e") {
+        output = output.replace('e', 3);
+    }
+    if (input[i] === "i") {
+        output = output.replace('i', 1);
+    }
+    if (input[i] === "o") {
+        output = output.replace('o', 0);
+    }
+    
+} console.log(output);
