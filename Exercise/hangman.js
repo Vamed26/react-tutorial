033aef7a3da6eb81a66a6977d5f331a800c57096
Vadym <vamed8926@gmail.com>
2020-05-03 08:34:01 +0300

    Update hangman.js

--- Exercise/hangman.js
+++ Exercise/hangman.js
@@ -5,41 +5,50 @@ var words = [
     "бамбук",
     "преферанс",
     "антифриз",
-    "кинолог" 
+    "кинолог"
 ];
 // Выбираем случайное слово
-var word=words[Math.floor(Math.random()*words.length)];
+var word = words[Math.floor(Math.random() * words.length)];
 //Создаем итоговый массив
 var answerArry = [];
-for (var i=0; i<word.length; i++) {
+for (var i = 0; i < word.length; i++) {
     answerArry[i] = "_";
 }
 var remainingLetters = word.length;
+var newRemainingLetters = remainingLetters;
 // remainingLetters - оставшиеся буквы
 //Игровой цикл
-while (remainingLetters>0) {
+var lives = 3;
+while ((remainingLetters > 0) && (lives !== 0)) {
     //Состояние игры
-    alert(answerArry.join(" "));
+    alert("Жизни: " + lives+"  "+ answerArry.join(" ")+"  "+"Осталось отгадать букв: "+ remainingLetters );
     // Запрашиваем вариант ответа
     var guess = prompt('Угадайте букву, или нажмите Отмена для выхода из игры!');
+    guess=guess.toLowerCase();
     // guess - предположение
     if (guess === null) {
         break;
     }
-else if (guess.length !==1) {
-    alert ("Пожалуйста введите одну букву!");
-}
-else {
-    guess.toUpperCase=guess;
-    //обновляем состояние игры
-    for ( var j=0; j<word.length; j++) {
-        if (word[j]===guess) {
-            answerArry[j] = guess;
-            remainingLetters--;
+    else if (guess.length !== 1) {
+        alert("Пожалуйста введите одну букву!");
+    }
+    else {
+        //обновляем состояние игры
+        for (var j = 0; j < word.length; j++) {
+            if (word[j] === guess) {
+                answerArry[j] = guess;
+                remainingLetters--;
 
+            }
         }
     }
+    if (remainingLetters === newRemainingLetters) {
+        lives--;
+    }
+    newRemainingLetters = remainingLetters;
+    if (lives === 0) {
+        alert("Не повезло, жизни закончились :(");
+    }
 }
-}
-alert (answerArry.join(" "));
-alert ("Отлично! Было загадано слово " + word);
+alert(answerArry.join(" "));
+alert("Было загадано слово: " + word);
